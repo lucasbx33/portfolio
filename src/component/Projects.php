cab9bb2ddc28356f4c8922e54194f38baa7805bb
LucasR <lucasreynaud4@gmail.com>
2023-06-06 16:13:55 +0200

    test comiit

--- src/component/Projects.php
+++ src/component/Projects.php
@@ -8,6 +8,15 @@
 				<p>P-fast est une application mobile qui recense le nombre de place disponible en temps réel dans les parkings bordelais. Les aires de covoiturage, les bornes éectriques ainsi que les places handicapés y sont aussi référencés</p>
 	  			<div class="voir-projet">
 				  	<p class="footer-text-project">Bientot disponible sur l'AppStore et le Google PlayStore</p>
+				</div>
+			</div>
+		</div>
+		<div class="project-item">
+			<img src="Crud_velo\assets\images\velo.png" alt="Projet 1 P-fast" class="img"/>
+			<div class="project-text">
+				<h3>CRUD</h3>
+				<p>Ce crud a été réalisé en début de première année en php, nous pouvos y créer des annonces en y ajoutant des titres, du texte ainsi que le mail de l'utilisateur, on peut modifier les données ainsi que les supprimer</p>
+	  			<div class="voir-projet">
 					<div class="footer-button-project">
 						<a href="https://crudvelo.lucasreynaud.fr"> Voir le projet</a>
 					</div>
